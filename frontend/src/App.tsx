d0558bf11e77a3a7901ce9e7947f450cf8c693f0
AdmiralB <gtiw@hotmail.com>
2022-07-05 12:45:10 +0100

    fixed top level hook bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,7 @@ import {
   AnimatedGrid,
   AnimatedAreaSeries,
   XYChart,
-  Tooltip,
 } from '@visx/xychart'
-import { useTooltip } from '@visx/tooltip'
 import * as curves from '@visx/curve'
 import './App.css'
 
@@ -22,12 +20,14 @@ const accessors = {
   yAccessor: (d: IChartValue) => d.Amount
 }
 
-const {
-  tooltipData,
-  tooltipOpen
-} = useTooltip();
+const formatCurrency = (amount: number) => {
+  return amount.toLocaleString('en-gb', {
+    style: 'currency',
+    currency: 'GBP'
+  });
+}
 
-function App() {
+const App = () => {
   const [principal, setPrincipal] = useState(500)
   const [interestRate, setInterestRate] = useState(3)
   const [amountValues, setAmountValues] = useState<InterestResponse>(new InterestResponse())
@@ -82,34 +82,13 @@ function App() {
         </div>
       </div>
       <br />
-      <XYChart height={500} width={800} xScale={{ type: 'band' }} yScale={{ type: 'linear' }}>
+      <XYChart height={400} width={800} xScale={{ type: 'band' }} yScale={{ type: 'linear' }}>
         <AnimatedAxis label='Years' orientation='bottom' />
         <AnimatedAxis label='Amount / £' orientation='left' labelOffset={22} />
         <AnimatedGrid rows columns numTicks={8} />
-        <AnimatedAreaSeries curve={curves.curveCardinal} fillOpacity={.4} dataKey={(interestRate + 2).toString() + "% Interest"} data={amountValues.plus_two_series} {...accessors} />
-        <AnimatedAreaSeries curve={curves.curveCardinal} fillOpacity={.4} dataKey={(interestRate + 1).toString() + "% Interest"} data={amountValues.plus_one_series} {...accessors} />
-        <AnimatedAreaSeries curve={curves.curveCardinal} fillOpacity={.4} dataKey={interestRate.toString() + "% Interest"} data={amountValues.main_series} {...accessors} />
-        {tooltipData !== undefined && (
-          <Tooltip
-          snapTooltipToDatumX
-          snapTooltipToDatumY
-          showVerticalCrosshair
-          showSeriesGlyphs
-          renderTooltip={({ tooltipData, colorScale }) => (
-            <div>
-              <div style={{ color: colorScale(tooltipData?.nearestDatum.key) }}>
-                {tooltipData.nearestDatum.key}
-              </div>
-              <div>
-                Year: {accessors.xAccessor(tooltipData.nearestDatum.datum)}
-              </div>
-              <div>
-                £{accessors.yAccessor(tooltipData.nearestDatum.datum)}
-              </div>
-            </div>
-          )}
-        />
-        )}
+        <AnimatedAreaSeries curve={curves.curveCardinal} fillOpacity={.6} dataKey={(interestRate + 2).toString() + "% Interest"} data={amountValues.plus_two_series} {...accessors} />
+        <AnimatedAreaSeries curve={curves.curveCardinal} fillOpacity={.6} dataKey={(interestRate + 1).toString() + "% Interest"} data={amountValues.plus_one_series} {...accessors} />
+        <AnimatedAreaSeries curve={curves.curveCardinal} fillOpacity={.6} dataKey={interestRate.toString() + "% Interest"} data={amountValues.main_series} {...accessors} />
       </XYChart>
       <div className='grid'>
         <div style={{ gridArea: 'interest-rate' }}>
@@ -134,55 +113,55 @@ function App() {
           <h3>{interestRate}%</h3>
         </div>
         <div style={{ gridArea: 'mr-1' }}>
-          <h3>{amountValues.main_series[1].Amount}</h3>
+          <h3>{formatCurrency(amountValues.main_series[1].Amount)}</h3>
         </div>
         <div style={{ gridArea: 'mr-2' }}>
-          <h3>{amountValues.main_series[2].Amount}</h3>
+          <h3>{formatCurrency(amountValues.main_series[2].Amount)}</h3>
         </div>
         <div style={{ gridArea: 'mr-5' }}>
-          <h3>{amountValues.main_series[5].Amount}</h3>
+          <h3>{formatCurrency(amountValues.main_series[5].Amount)}</h3>
         </div>
         <div style={{ gridArea: 'mr-10' }}>
-          <h3>{amountValues.main_series[10].Amount}</h3>
+          <h3>{formatCurrency(amountValues.main_series[10].Amount)}</h3>
         </div>
         <div style={{ gridArea: 'mr-20' }}>
-          <h3>{amountValues.main_series[20].Amount}</h3>
+          <h3>{formatCurrency(amountValues.main_series[20].Amount)}</h3>
         </div>
         <div style={{ gridArea: 'plus-1-rate' }}>
           <h3>{interestRate + 1}%</h3>
         </div>
         <div style={{ gridArea: 'p1-1' }}>
-          <h3>{amountValues.plus_one_series[1].Amount}</h3>
+          <h3>{formatCurrency(amountValues.plus_one_series[1].Amount)}</h3>
         </div>
         <div style={{ gridArea: 'p1-2' }}>
-          <h3>{amountValues.plus_one_series[2].Amount}</h3>
+          <h3>{formatCurrency(amountValues.plus_one_series[2].Amount)}</h3>
         </div>
         <div style={{ gridArea: 'p1-5' }}>
-          <h3>{amountValues.plus_one_series[5].Amount}</h3>
+          <h3>{formatCurrency(amountValues.plus_one_series[5].Amount)}</h3>
         </div>
         <div style={{ gridArea: 'p1-10' }}>
-          <h3>{amountValues.plus_one_series[10].Amount}</h3>
+          <h3>{formatCurrency(amountValues.plus_one_series[10].Amount)}</h3>
         </div>
         <div style={{ gridArea: 'p1-20' }}>
-          <h3>{amountValues.plus_one_series[20].Amount}</h3>
+          <h3>{formatCurrency(amountValues.plus_one_series[20].Amount)}</h3>
         </div>
         <div style={{ gridArea: 'plus-2-rate' }}>
           <h3>{interestRate + 2}%</h3>
         </div>
         <div style={{ gridArea: 'p2-1' }}>
-          <h3>{amountValues.plus_two_series[1].Amount}</h3>
+          <h3>{formatCurrency(amountValues.plus_two_series[1].Amount)}</h3>
         </div>
         <div style={{ gridArea: 'p2-2' }}>
-          <h3>{amountValues.plus_two_series[2].Amount}</h3>
+          <h3>{formatCurrency(amountValues.plus_two_series[2].Amount)}</h3>
         </div>
         <div style={{ gridArea: 'p2-5' }}>
-          <h3>{amountValues.plus_two_series[5].Amount}</h3>
+          <h3>{formatCurrency(amountValues.plus_two_series[5].Amount)}</h3>
         </div>
         <div style={{ gridArea: 'p2-10' }}>
-          <h3>{amountValues.plus_two_series[10].Amount}</h3>
+          <h3>{formatCurrency(amountValues.plus_two_series[10].Amount)}</h3>
         </div>
         <div style={{ gridArea: 'p2-20' }}>
-          <h3>{amountValues.plus_two_series[20].Amount}</h3>
+          <h3>{formatCurrency(amountValues.plus_two_series[20].Amount)}</h3>
         </div>
       </div>
     </div>
